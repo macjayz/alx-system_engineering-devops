7a079356eef8cce2caa7f7f84cffe3af61b7c82c
macjayz <macjayzz1@gmail.com>
2023-05-07 07:05:37 -0700

    New commit added

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # regular expression that matches a given pattern
 string = ARGV[0]
-puts string.scan(/\[from:(.*)\] \[to:(.*)\] \[flags:(.*)\]/).join(",")
+puts string.scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
